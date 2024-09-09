6a12904f31c93b398dc8c018425107485f40e7bb
darkessence87 <github.com/darkessence87>
2024-09-09 19:31:58 +0200

    thread runner update

--- psi/include/psi/thread/ThreadRunner.h
+++ psi/include/psi/thread/ThreadRunner.h
@@ -1,10 +1,11 @@
 #pragma once
 
 #include <functional>
+#include <thread>
 
 #include "CrashHandler.h"
 
-#include "common/logger/Logger.h"
+#include "psi/logger/Logger.h"
 
 class ThreadRunner
 {
@@ -18,26 +19,40 @@ public:
     {
     }
 
+    ~ThreadRunner()
+    {
+        if (m_thread.joinable()) {
+            m_thread.join();
+        }
+    }
+
     void run(OnCrashFn &&onCrashFn)
     {
-        m_onCrashSub =
-            m_crashHandler.crashEvent().subscribe([this, onCrashFn](const auto &errorMsg, const auto &stacktrace) {
-                LOG_ERROR("Crash in " << m_threadName << " thread, [" << errorMsg << "]");
-                LOG_ERROR(stacktrace);
-                onCrashFn();
+        if (m_thread.joinable()) {
+            m_thread.join();
+        }
+
+        m_thread = std::thread([=, this]() {
+            m_onCrashSub =
+                m_crashHandler.crashEvent().subscribe([this, onCrashFn](const auto &errorMsg, const auto &stacktrace) {
+                    LOG_ERROR("Crash in " << m_threadName << " thread, [" << errorMsg << "]");
+                    LOG_ERROR(stacktrace);
+                    onCrashFn();
+                });
+
+            m_crashHandler.invoke([this]() {
+                LOG_INFO("Run " << m_threadName);
+                m_runFn();
             });
 
-        m_crashHandler.invoke([this]() {
-            LOG_INFO("Run " << m_threadName);
-            m_runFn();
+            m_onCrashSub.reset();
         });
-
-        m_onCrashSub.reset();
     }
 
 private:
     const std::string m_threadName;
-    psi::CrashHandler m_crashHandler;
-    psi::Subscription m_onCrashSub;
+    std::thread m_thread;
+    psi::thread::CrashHandler m_crashHandler;
+    psi::comm::Subscription m_onCrashSub;
     RunFn m_runFn;
 };
